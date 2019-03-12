92deda23a6d8211b2ef76d8652a0b136e28e4836
jeremy <jbarnesspain@gmail.com>
2019-03-12 14:44:19 +0100

    changes to training script

--- script/train_classifier.sh
+++ script/train_classifier.sh
@@ -12,10 +12,10 @@ for lang in es ca; do
         for i in 1 2 3 4 5; do
 
             # train biLSTM
-            python3 artetxe_bilstm.py -l $lang -se "$source_embeddings" -te "$target_embedding_dir"/sg-300-"$lang".txt -b "$binary" -td datasets/training/"$lang"/raw/ -seed "$i"
+            python3 artetxe_bilstm.py -l $lang -se "$source_embeddings" -te "$target_embedding_dir"/sg-300-"$lang".txt -b "$binary" -td datasets/training/"$lang"/raw/
 
             # train CNN
-            python3 artetxe_cnn.py -l $lang -se "$source_embeddings" -te "$target_embedding_dir"/sg-300-"$lang".txt -b "$binary" -td datasets/training/"$lang"/raw/ -seed "$i"
+            python3 artetxe_cnn.py -l $lang -se "$source_embeddings" -te "$target_embedding_dir"/sg-300-"$lang".txt -b "$binary" -td datasets/training/"$lang"/raw/
         done;
     done;
 done;
