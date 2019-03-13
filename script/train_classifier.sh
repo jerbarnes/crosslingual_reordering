d8821dba90f65414325ce9f9579f3f53606222d0
jeremy <jbarnesspain@gmail.com>
2019-03-13 12:49:10 +0100

    reset training script

--- script/train_classifier.sh
+++ script/train_classifier.sh
@@ -9,13 +9,12 @@ for lang in es ca; do
         # train SVM
         python3 artetxe_svm.py -l $lang -se "$source_embeddings" -te "$target_embedding_dir"/sg-300-"$lang".txt -b "$binary" -td datasets/training/"$lang"/raw/
 
-        for i in 1 2 3 4 5; do
 
-            # train biLSTM
-            python3 artetxe_bilstm.py -l $lang -se "$source_embeddings" -te "$target_embedding_dir"/sg-300-"$lang".txt -b "$binary" -td datasets/training/"$lang"/raw/
+        # train biLSTM
+        python3 artetxe_bilstm.py -l $lang -se "$source_embeddings" -te "$target_embedding_dir"/sg-300-"$lang".txt -b "$binary" -td datasets/training/"$lang"/raw/
 
-            # train CNN
-            python3 artetxe_cnn.py -l $lang -se "$source_embeddings" -te "$target_embedding_dir"/sg-300-"$lang".txt -b "$binary" -td datasets/training/"$lang"/raw/
+        # train CNN
+        python3 artetxe_cnn.py -l $lang -se "$source_embeddings" -te "$target_embedding_dir"/sg-300-"$lang".txt -b "$binary" -td datasets/training/"$lang"/raw/
         done;
     done;
 done;
